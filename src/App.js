6a6034f10d5be2d300cbfc39d7b1ee3dafb7b07a
Ben Reitman <ben.reitman@gmail.com>
2020-07-15 15:11:17 -0700

    fixing heroku error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {NavBar} from './components'
 import Routes from './routes'
 
 import {library} from '@fortawesome/fontawesome-svg-core'
-// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {fab} from '@fortawesome/free-brands-svg-icons'
 import {faFileAlt} from '@fortawesome/free-solid-svg-icons'
 
